a48d97f526dccd7d06b9e14a8edaa710411ee58e
vrichommeyuoou@tyutyu <v.richomme@gmail.com>
2013-05-28 22:51:09 +0200

    work in progress

---
+++ XibParser/Foundation/NSNotificationCenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smartmobili.Cocoa
+{
+    public class NSNotificationCenter : NSObject
+    {
+        new public static Class Class = new Class(typeof(NSNotificationCenter));
+        new public static NSNotificationCenter Alloc() { return new NSNotificationCenter(); }
+
+        private static NSNotificationCenter default_center = null;
+
+
+        static NSNotificationCenter() { Initialize(); }
+        static void Initialize()
+        {
+            default_center = (NSNotificationCenter)NSNotificationCenter.Alloc().Init();
+        }
+
+
+        public static NSNotificationCenter DefaultCenter
+        {
+            get { return default_center; }
+        }
+    }
+}
