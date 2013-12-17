245e5576a44c7c6f8d7b46a9240a0c7e0fe7498a
vrichommeyuoou@tyutyu <v.richomme@gmail.com>
2013-12-17 23:07:20 +0100

    wip

--- XibParser/Foundation/NSXMLDTD.cs
+++ XibParser/Foundation/NSXMLDTD.cs
@@ -9,5 +9,20 @@ namespace Smartmobili.Cocoa
     {
         new public static Class Class = new Class(typeof(NSXMLDTD));
         new public static NSXMLDTD alloc() { return new NSXMLDTD(); }
+
+        protected NSString _name;
+        protected NSString _publicID;
+        protected NSString _systemID;
+        protected NSXMLChildren _children;
+        protected bool _childrenHaveMutated;
+        protected NSMutableDictionary _entities;
+        protected NSMutableDictionary _elements;
+        protected NSMutableDictionary _notations;
+        protected NSMutableDictionary _attributes;
+        protected NSString _original;
+        protected bool _modified;
+
+
+
     }
 }
