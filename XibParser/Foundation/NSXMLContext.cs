459c4e29f7d32830310f2c6cc571bf56fdabf533
vrichommeyuoou@tyutyu <v.richomme@gmail.com>
2013-12-17 01:17:41 +0100

    wip

---
+++ XibParser/Foundation/NSXMLContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smartmobili.Cocoa
+{
+    public class NSXMLContext : NSObject
+    {
+        new public static Class Class = new Class(typeof(NSXMLContext));
+        new public static NSXMLContext alloc() { return new NSXMLContext(); }
+
+        public static NSString stringForObjectValue(id anObject)
+        {
+            return "";
+        }
+
+    }
+}
